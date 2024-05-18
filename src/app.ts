fe55b78d32272f7be4ceb79f7e5381ce733511e0
Leticia Diaz <leticiadiaz512@gmail.com>
2024-05-17 23:53:40 -0600

    :sparkles: FEAT: different answers

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,25 @@
+import fs from 'fs';
 import http from 'http'
 
 const server = http.createServer((req, res) => {
     console.log(req.url);
     //res.write('Hola mundo');
-   
-    res.writeHead(400, {'Content-Type': 'text/html'});
-    res.write('<h1>Hola mundo</h1>')
-    res.end();
+    // res.writeHead(400, {'Content-Type': 'text/html'});
+    // res.write(`<h1>URL ${req.url}</h1>`)
+    // res.end();
+
+    // const data = { name: 'Jhon Doe', age: 30, city: 'New York'};
+    // res.writeHead(200, {'Content-Type': 'application/json'});
+    // res.end(JSON.stringify(data));
+
+    if (req.url === '/'){
+        const htmlFile = fs.readFileSync('./public/index.html','utf-8');
+        res.writeHead(400, {'Content-Type': 'text/html'});
+        res.end(htmlFile)
+    }else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end();
+    }
 })
 
 server.listen(8081, () =>{
