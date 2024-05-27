adfd0baa9cd85fdb42423f5e4a5400547b35c5d9
Leticia Diaz <leticiadiaz512@gmail.com>
2024-05-26 18:35:46 -0600

    :boom: FEAT: add DTOs

--- src/presentation/todos/controller.ts
+++ src/presentation/todos/controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { prisma } from '../../data/postgres';
+import { CreateTodoDto } from '../../domain/dtos';
 
 export class TodosController {
 
@@ -23,11 +24,12 @@ export class TodosController {
     };
 
     public createTodo = async (req: Request, res: Response) => {
-        const { text } = req.body;
-        if ( !text ) return res.status(400).json({ error: 'Text property is required'});
+
+        const [error, createTodoDto] = CreateTodoDto.create(req.body);
+        if (error) return res.status(400).json({error});
 
         const todo = await prisma.todo.create({
-            data: { text: text}
+            data:  createTodoDto!
         });
 
         res.json(todo)
